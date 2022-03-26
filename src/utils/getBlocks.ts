d1186319579491b2160f60b4d342003eaf97b995
Tim Shaker <tim@tmshkr.com>
2022-03-25 22:50:50 -0700

    move submodule

--- src/utils/getBlocks.ts
+++ src/utils/getBlocks.ts
@@ -3,27 +3,24 @@ const lineReader = require("line-reader");
 
 export const getBlocks = async (filePath: string) => {
   return new Promise((resolve, reject) => {
-    lineReader.eachLine(
-      path.join(__dirname, `../content/${filePath}`),
-      function (line, last) {
-        const urlPrefix = "https://app.slack.com/block-kit-builder/T04502KQX#";
-        if (line.startsWith(urlPrefix)) {
-          try {
-            const json = JSON.parse(decodeURI(line.split(urlPrefix)[1]));
-            resolve({
-              blocks: json.blocks,
-              text: json.blocks.find((el) => el.text?.text)?.text.text,
-            });
-          } catch (err) {
-            console.error(err);
-            reject("Invalid JSON");
-          }
-          return false;
+    const urlPrefix = "https://app.slack.com/block-kit-builder/T04502KQX#";
+    lineReader.eachLine(`content/${filePath}`, function (line, last) {
+      if (line.startsWith(urlPrefix)) {
+        try {
+          const json = JSON.parse(decodeURI(line.split(urlPrefix)[1]));
+          resolve({
+            blocks: json.blocks,
+            text: json.blocks.find((el) => el.text?.text)?.text.text,
+          });
+        } catch (err) {
+          console.error(err);
+          reject("Invalid JSON");
         }
-        if (last) {
-          reject("Blocks not found");
-        }
+        return false;
       }
-    );
+      if (last) {
+        reject("Blocks not found");
+      }
+    });
   });
 };
