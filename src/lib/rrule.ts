53ef881c806e6430cc741bcf73acf4c84017e53e
Tim Shaker <tim@tmshkr.com>
2022-04-18 16:23:36 -0700

    rrule workaround

--- src/lib/rrule.ts
+++ src/lib/rrule.ts
@@ -5,18 +5,18 @@ import dayjs from "./dayjs";
 Returns the given date as if it were UTC, replacing any timezone offset.
 Workaround for https://github.com/jakubroztocil/rrule/issues/501
 */
-export function getFakeUTC(date: dayjs.Dayjs) {
+export function getFakeUTC(date: dayjs.Dayjs): string {
   return date.format().slice(0, -6) + "Z";
 }
 
-export function getNextOccurrence(rrule: string) {
+export function getNextOccurrence(rrule: string): string {
   const rule = rrulestr(rrule);
   const maxDate = new Date(8640000000000000);
 
   const [nextOccurrence] = rule.between(
     new Date(getFakeUTC(dayjs())),
     maxDate,
-    true,
+    false,
     (date, i) => i === 0
   );
 
