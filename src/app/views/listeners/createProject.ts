ce1a11ab21ac917cf312aeffea884d3bd7fb4f40
Tim Shaker <tim@tmshkr.com>
2022-05-23 15:25:28 -0700

    add team members

--- src/app/views/listeners/createProject.ts
+++ src/app/views/listeners/createProject.ts
@@ -1,20 +1,32 @@
 import prisma from "lib/prisma";
 import { getHomeTab } from "app/views/home";
+import { getInnerValues } from "utils/getInnerValues";
 
 export const createProject = async ({ ack, body, view, client, logger }) => {
   await ack();
-  const { new_project_title } = view.state.values.new_project_title_block;
-  console.log(new_project_title);
+
+  const values = getInnerValues(view.state.values);
+  const { new_project_title, team_members } = values;
+  console.log(values);
 
   await prisma.project.create({
     data: {
       name: new_project_title.value,
       created_by: body.user.id,
       team_members: {
-        create: {
-          slack_id: body.user.id,
-          role: "OWNER",
-        },
+        create: team_members.selected_conversations.map((slack_id) => {
+          if (slack_id === body.user.id) {
+            return {
+              slack_id,
+              role: "OWNER",
+            };
+          } else {
+            return {
+              slack_id,
+              role: "MEMBER",
+            };
+          }
+        }),
       },
     },
   });
